81dc0876ced4b02fba7ad57c9a82a02eca727513
dwikoadya <dwikoadya@gmail.com>
2022-07-02 14:55:07 +0700

    Struktur Organisasi

--- src/components/StrukturOrganisasi.tsx
+++ src/components/StrukturOrganisasi.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
 export default function StrukturOrganisasi() {
   return (
-    <div className="flex h-[917px]">
-      Struktur Organisasi
+    <div className="flex flex-col h-[917px] justify-center items-center">
+      <div className="text-center ">
+        <h3 className="font-bold text-4xl m-9 tracking-wider">Struktur Organisasi</h3>
+        <Image
+          alt="struktur"
+          className="object-cover"
+          height={667}
+          quality="100"
+          src="/images/struktur.png"
+          width={1296}
+        />
+      </div>
     </div>
   );
 }
